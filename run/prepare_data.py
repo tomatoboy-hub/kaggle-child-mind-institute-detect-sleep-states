ffa00da25404d4d317d5699b1769ebf1a8cc4531
tomatoboy <hayatarou.ay@gmail.com>
2023-11-11 02:02:17 +0900

    2times range

--- run/prepare_data.py
+++ run/prepare_data.py
@@ -32,6 +32,12 @@ FEATURE_NAMES = [
     "anglez_cos",
     "season_sin",
     "season_cos",
+    "hour_2_sin_2",
+    "hour_2_cos_2",
+    "month_sin_2",
+    "month_cos_2",
+    "minute_sin_2",
+    "minute_cos_2",
 ]
 
 ANGLEZ_MEAN = -8.810476
@@ -47,6 +53,12 @@ def to_coord(x: pl.Expr, max_: int, name: str) -> list[pl.Expr]:
 
     return [x_sin.alias(f"{name}_sin"), x_cos.alias(f"{name}_cos")]
 
+def to_coord_2(x: pl.Expr, max_: int, name: str) -> list[pl.Expr]:
+    rad = np.pi * (x % max_) / max_
+    x_sin = rad.sin()
+    x_cos = rad.cos()
+
+    return [x_sin.alias(f"{name}_sin_2"), x_cos.alias(f"{name}_cos_2")]
 
 def deg_to_rad(x: pl.Expr) -> pl.Expr:
     return np.pi / 180 * x
@@ -89,13 +101,15 @@ def add_seasonal_features(series_df: pl.DataFrame) -> pl.DataFrame:
     ])
 
 def add_feature(series_df: pl.DataFrame) -> pl.DataFrame:
-    series_df = add_seasonal_features(series_df)
     series_df = (
         series_df.with_row_count("step")
         .with_columns(
             *to_coord(pl.col("timestamp").dt.hour(), 24, "hour"),
             *to_coord(pl.col("timestamp").dt.month(), 12, "month"),
             *to_coord(pl.col("timestamp").dt.minute(), 60, "minute"),
+            *to_coord_2(pl.col("timestamp").dt.hour(), 24, "hour_2"),
+            *to_coord_2(pl.col("timestamp").dt.month(), 12, "month_2"),
+            *to_coord_2(pl.col("timestamp").dt.minute(), 60, "minute_2"),
             pl.col("step") / pl.count("step"),
             pl.col('anglez_rad').sin().alias('anglez_sin'),
             pl.col('anglez_rad').cos().alias('anglez_cos'),
